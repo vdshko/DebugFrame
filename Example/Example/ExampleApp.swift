ab8c6690ff9c2de52d5161bc7004d4fefa90691a
Vladyslav Shkodych <vladyslav.shkodych@jmind.systems>
2022-12-19 13:55:57 +0200

    Initial project setup

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Vlad Shkodich on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Source/DebugFrame/DebugFrame.h
@@ -0,0 +1,18 @@
+//
+//  DebugFrame.h
+//  DebugFrame
+//
+//  Created by Vlad Shkodich on 19.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DebugFrame.
+FOUNDATION_EXPORT double DebugFrameVersionNumber;
+
+//! Project version string for DebugFrame.
+FOUNDATION_EXPORT const unsigned char DebugFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebugFrame/PublicHeader.h>
+
+
