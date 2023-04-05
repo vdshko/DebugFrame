61a7222a0947bb2ebbd50ac6a9e7278bc1e92f81
Vladyslav Shkodych <vladyslav.shkodych@gmail.com>
2023-04-06 02:41:15 +0300

    [add] comments for public methods

--- Source/DebugFrame/DebugFrame.swift
+++ Source/DebugFrame/DebugFrame.swift
@@ -5,6 +5,16 @@
 //  Created by Vladyslav Shkodych on 19.12.2022.
 //
 
+/*
+ DebugFrame is an overlay that can be appended to your view, for debugging its origin and size.
+ 
+ DebugFrame will not be compiled in the release build, so you may not worry if you forget to remove it from some of your view.
+ To keep your code as clean as possible, it's probably a good idea to add a SwiftLint or any other linting rule to make an Xcode warning for this.
+ 
+ There are two methods for modifying your view with debugFrame overlay.
+ The two methods need only for easier use of the Xcode autocomplete.
+ */
+
 import Foundation
 import SwiftUI
 
@@ -70,11 +80,28 @@ public enum DebugFrameOutput {
 
 public extension View {
     
+    /// Applies an overlay to your view and returns a new view. The overlay contains a border of your view, its origin, and size.
+    /// - Parameter color: Default color for the border of the overlay and outputs.
+    /// - Returns: View that uses the debug overlay.
+    func debugFrame(color: Color = Color.red) -> some View {
+        return modifier(
+            DebugFrame(
+                color: color,
+                outputs: [DebugFrameOutput.all]
+            )
+        )
+    }
+    
+    /// Applies an overlay to your view and returns a new view. The overlay contains a border of your view, its origin, and size.
+    /// - Parameters:
+    ///   - color: Default color for the border of the overlay and outputs.
+    ///   - outputs: Default outputs of the overlay such as origin and size.
+    /// - Returns: View that uses the debug overlay.
     func debugFrame(color: Color = Color.red, _ outputs: DebugFrameOutput...) -> some View {
         return modifier(
             DebugFrame(
                 color: color,
-                outputs: outputs.isEmpty ? [.all] : outputs
+                outputs: outputs.isEmpty ? [DebugFrameOutput.all] : outputs
             )
         )
     }
