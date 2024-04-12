3b7d9360569f9ff6d5d5fd0e633b8eedf595dee3
Vladyslav Shkodych <vladyslav.shkodych@jmind.systems>
2024-04-13 01:59:54 +0300

    [add] new debugBackground extension to the View & [add] Mac OS support for the lib

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
                 .frame(width: 100, height: 100)
                 .foregroundColor(Color.red)
         }
+//        .debugBackground() // uncomment to see in work
     }
     
     private var rectangles: some View {
--- Source/DebugFrame/DebugFrame.swift
+++ Source/DebugFrame/DebugFrame.swift
@@ -7,10 +7,10 @@
 
 /*
  DebugFrame is an overlay that can be appended to your view, for debugging its origin and size.
- 
+
  DebugFrame will not be compiled in the release build, so you may not worry if you forget to remove it from some of your view.
  To keep your code as clean as possible, it's probably a good idea to add a SwiftLint or any other linting rule to make an Xcode warning for this.
- 
+
  There are two methods for modifying your view with debugFrame overlay.
  The two methods need only for easier use of the Xcode autocomplete.
  */
@@ -18,37 +18,47 @@
 import Foundation
 import SwiftUI
 
+// MARK: - DebugFrame ViewModifier
+
 private struct DebugFrame: ViewModifier {
-    
+
+    // MARK: - Properties
+
     let color: Color
     let outputs: [DebugFrameOutput]
-    
+
+    // MARK: - Body
+
     func body(content: Content) -> some View {
         #if DEBUG
-            content
-                .overlay(
-                    GeometryReader { geometry in
-                        ZStack(alignment: .bottom) {
-                            Rectangle()
-                                .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [5]))
-                                .foregroundColor(color)
-                            Text(defineOutput(with: geometry))
-                                .foregroundColor(color)
-                                .font(.caption2)
-                        }
-                    }
-                )
+        content.overlay(overlayView)
         #else
-            content
+        content
         #endif
     }
-    
+
+    // MARK: - Private methods
+
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .bottom) {
+                Rectangle()
+                    .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [5]))
+                    .foregroundColor(color)
+                Text(defineOutput(with: geometry))
+                    .foregroundColor(color)
+                    .font(.caption2)
+            }
+        }
+    }
+
     private func defineOutput(with geometry: GeometryProxy) -> String {
         let globalOrigin: CGPoint = geometry.frame(in: .global).origin
         let originX: String = "x: \(rounded(globalOrigin.x))"
         let originY: String = "y: \(rounded(globalOrigin.y))"
         let width: String = "w: \(rounded(geometry.size.width))"
         let height: String = "h: \(rounded(geometry.size.height))"
+        
         return String(
             outputs.reduce(into: String()) {
                 switch $1 {
@@ -64,22 +74,36 @@ private struct DebugFrame: ViewModifier {
             }.dropLast(3)
         )
     }
-    
+
     private func rounded(_ value: CGFloat) -> Float {
         return Float(round(100 * value) / 100)
     }
 }
 
+// MARK: - DebugFrameOutput enum
+
 public enum DebugFrameOutput {
-    
+
+    /// (originX, originY) | (width, height)
     case all
-    case origin, size
-    case originX, originY
-    case width, height
+    /// (originX, originY)
+    case origin
+    /// (width, height)
+    case size
+    /// originX
+    case originX
+    /// originY
+    case originY
+    /// width
+    case width
+    /// height
+    case height
 }
 
+// MARK: - ViewExtensions
+
 public extension View {
-    
+
     /// Applies an overlay to your view and returns a new view. The overlay contains a border of your view, its origin, and size.
     /// - Parameter color: Default color for the border of the overlay and outputs.
     /// - Returns: View that uses the debug overlay.
@@ -91,7 +115,7 @@ public extension View {
             )
         )
     }
-    
+
     /// Applies an overlay to your view and returns a new view. The overlay contains a border of your view, its origin, and size.
     /// - Parameters:
     ///   - color: Default color for the border of the overlay and outputs.
@@ -104,5 +128,24 @@ public extension View {
                 outputs: outputs.isEmpty ? [DebugFrameOutput.all] : outputs
             )
         )
+    }
+    
+    /// Applies a background, with random Color, to your view and returns a new view.
+    ///
+    /// Each time SwiftUI re-creates the body for the view with this background, the Color will be picked randomly.
+    /// - Returns: View with the re-rendered background Color.
+    func debugBackground() -> some View {
+        #if DEBUG
+        background(
+            Color(
+                red: .random(in: 0...1),
+                green: .random(in: 0...1),
+                blue: .random(in: 0...1),
+                opacity: 0.8
+            )
+        )
+        #else
+        self
+        #endif
     }
 }
