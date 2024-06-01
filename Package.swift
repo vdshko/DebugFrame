c1e3d53e47560e4d02f0f4ff449d00f7ede825aa
Vladyslav Shkodych <vladyslav.shkodych@gmail.com>
2024-06-01 13:31:15 +0300

    add: missed macos support for the podspec and package files

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "DebugFrame",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v10_15)
     ],
     products: [
         .library(
